dfd63473b5d1d6ef6a4216338838e6af96e41913
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-10-18 11:24:34 +0700

    mind grenade in module

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,7 +1,7 @@
 const sayHi = require('./3-utils.js');
 const names = require('./1-names.js');
 const data = require('./4.alternative-flavor.js');
-
-console.log(data);
-sayHi(names.john);
-sayHi('Dave');
+require('./5-mind-grenade.js');
+// console.log(data);
+// sayHi(names.john);
+// sayHi('Dave');
