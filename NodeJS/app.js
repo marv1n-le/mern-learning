e687aeae0ec22ec9df6684e9152a6f2f30697e07
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-10-21 23:16:28 +0700

    events emitter

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,38 +1,13 @@
-const { read } = require('fs');
+const EventEmitter = require('events');
 
-const { readFile, writeFile } = require('fs').promises;
+const customEmitter = new EventEmitter();
 
-// const start = async () => {
-//   try {
-//     const first = await readFile('./content/first.txt', 'utf8');
-//     const second = await readFile('./content/second.txt', 'utf8');
-//     await writeFile(
-//       './content/result-mind-grenade.txt',
-//       `Here is the result: ${first}, ${second}`,
-//       { flag: 'a' }
-//     );
-//     console.log('File written successfully');
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// }
+customEmitter.on('response', () => {
+  console.log(`data received`);
+});
 
-// start();
+customEmitter.on("response", () => {
+  console.log(`some other logic here`);
+});
 
-
-const start = async () => {
-  try {
-    const first = await readFile('./content/first.txt', 'utf8');
-    const second = await readFile('./content/second.txt', 'utf8');
-    await writeFile(
-      './content/result-mind-grenade.txt',
-      `Here is the result: ${first}, ${second}`,
-      { flag: 'a' }
-    );
-    console.log('File written successfully');
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-start();
+customEmitter.emit('response');
