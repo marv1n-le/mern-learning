a07b5734f217b0758b6bcb21117bdb55cdfe55d2
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-10-25 13:26:57 +0700

    route params

--- NodeJS/02-express-tutorial/app.js
+++ NodeJS/02-express-tutorial/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'navbar-app')));
+const { products } = require('./data');
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'navbar-app', 'index.html'));
+  res.send('<h1>Home Page</h1> <a href="/api/products">API Products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((products) => {
+    const { id, name, image } = products;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const singleProduct = products.find((product) => product.id === Number(id));
+  if(!singleProduct){
+    return res.status(404).send('Product Not Found');
+  }
+  res.json(singleProduct);
+});
+
+app.get('/api/products/:id/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('Hello World');
 });
 
 app.listen(3000, () => {
