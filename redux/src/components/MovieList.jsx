da0d9845c2c5e74c716fbc6f09699764eea58558
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-10-27 18:09:38 +0700

    implementing remove movie funtion

--- redux/src/components/MovieList.jsx
+++ redux/src/components/MovieList.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { removeMovie } from "../movieSlice";
+import { useDispatch } from "react-redux";
 
 export const MovieList = () => {
   const movies = useSelector((state) => state.movies.movies);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Movie List</h1>
       {movies.map((movie) => (
-        <p key={movie.id}>{movie.name}</p>
+        <p key={movie.id}>
+          {movie.name}
+          <button onClick={() => {
+            dispatch(removeMovie(movie.id));
+          }}>Delete Movie</button>
+        </p>
       ))}
     </div>
   );
--- redux/src/movieSlice.js
+++ redux/src/movieSlice.js
@@ -16,14 +16,15 @@ const movieSlice = createSlice({
   initialState,
   reducers: {
     addMovie: (state, action) => {
-      const newMovie = { 
-        id: state.movies[state.movies.length - 1].id + 1,
+      const lastMovie = state.movies[state.movies.length - 1];
+      const newMovie = {
+        id: lastMovie ? lastMovie.id + 1 : 1,
         name: action.payload
-      }
+      };
       state.movies.push(newMovie);
     },
     removeMovie: (state, action) => {
-      
+      state.movies = state.movies.filter(movie => movie.id !== action.payload);
     }
   }
 });
